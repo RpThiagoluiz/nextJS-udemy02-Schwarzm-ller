66745271bc0600106390cc7526a8dd1bcf2e415d
thiago <rp.thiagoluiz@gmail.com>
2021-07-13 12:04:01 -0300

    finish & upt 404 not found

--- Next/pages/clients/[clientId]/index.jsx
+++ Next/pages/clients/[clientId]/index.jsx
@@ -4,7 +4,24 @@ import { useRouter } from "next/router";
 const ClientProjects = () => {
   const router = useRouter();
   console.log(router.query); //{clientId: "thiago"}
-  return <h1>The Projects of a Given Client</h1>;
+
+  const handleLoadProjects = () => {
+    //Load data...
+    //router.push("/clients/chico/mijarnacozinha"); //Navegacao pragmatica
+    //Tbm pode passar Alternativa
+    router.push({
+      //query no lugar de um nome estatico, pode passar nome dinamic, que pode ser pego pelo pathname de router, useRouter()
+      pathname: "/clients/[clientId]/[clientprojectid]",
+      query: { clientId: "chico", clientprojectid: "mijarnacozinha" },
+    });
+  };
+
+  return (
+    <>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={handleLoadProjects}> Load Project A</button>
+    </>
+  );
 };
 
 export default ClientProjects; //http://localhost:3000/clients/thiago
--- Next/pages/index.jsx
+++ Next/pages/index.jsx
@@ -1,5 +1,28 @@
+import Link from "next/link"; //default export
+
 const Home = () => {
-  return <h1>The Home Page</h1>;
+  return (
+    <>
+      <h1>The Home Page</h1>
+      <p>In next, we have Link. </p>
+      <span>A vs Link</span>
+      <p>A - request para carregar a pagina</p>
+      <p>
+        Link - vai pegar diretamente sem fazer a request melhorando a otimizacao
+        do site. Ele deixa ela pre pronta no backend do next. Evitando q ela
+        carregue.
+      </p>
+
+      <ul>
+        <li>
+          <a href="/portifolio">Portifolio</a>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 //All pages need export default

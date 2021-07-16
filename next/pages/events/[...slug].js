58c3596ee64e59b28bf8d078bc3526db25bb75f0
thiago <rp.thiagoluiz@gmail.com>
2021-07-16 11:29:37 -0300

    finish

--- next/pages/events/[...slug].js
+++ next/pages/events/[...slug].js
@@ -1,20 +1,71 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
+import { useRouter } from "next/router";
 
-import { getFilteredEvents } from '../../dummy-data';
-import EventList from '../../components/events/event-list';
-import ResultsTitle from '../../components/events/results-title';
-import Button from '../../components/ui/button';
-import ErrorAlert from '../../components/ui/error-alert';
+//import { getFilteredEvents } from '../../dummy-data';
+import { getFilteredEvents } from "../../helpers/api-util";
+import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
-function FilteredEventsPage() {
+function FilteredEventsPage(props) {
+  //Nao ta precisando client-side data Fetching, contudo seria bom para os mecanimos de buscas.
+  const {
+    hasError,
+    filteredEvents,
+    date: { year, month },
+  } = props;
   const router = useRouter();
-
   const filterData = router.query.slug;
 
   if (!filterData) {
-    return <p className='center'>Loading...</p>;
+    return <p className="center">Loading...</p>;
   }
+
+  if (hasError) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(year, month - 1);
+
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
+  );
+}
+
+//como tem muitos eventos acontecendo, e diferente posibilidades de filtros, vamos utilizar o server side props.
+
+export const getServerSideProps = async (context) => {
+  //pegar os params da rota acessada.
+  const { params } = context;
+  //filterData ele vai vem pelos parametros da rota, que sao passados e veem do nosso filtro.
+
+  const filterData = params.slug; //slug vem do nome dos arquivos das rotas.
 
   const filteredYear = filterData[0];
   const filteredMonth = filterData[1];
@@ -30,44 +81,28 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return (
-      <Fragment>
-        <ErrorAlert>
-          <p>Invalid filter. Please adjust your values!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </Fragment>
-    );
+    return {
+      props: { hasError: true },
+      // notFound: true,
+      // redirect: {
+      //   destination: '/error'
+      // }
+    }; //retorno de um 404 tbm pode retornar um rediret aq.
   }
 
-  const filteredEvents = getFilteredEvents({
+  const date = {
     year: numYear,
     month: numMonth,
-  });
+  };
 
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <Fragment>
-        <ErrorAlert>
-          <p>No events found for the chosen filter!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
-        </div>
-      </Fragment>
-    );
-  }
+  const filteredEvents = await getFilteredEvents(date);
 
-  const date = new Date(numYear, numMonth - 1);
-
-  return (
-    <Fragment>
-      <ResultsTitle date={date} />
-      <EventList items={filteredEvents} />
-    </Fragment>
-  );
-}
+  return {
+    props: {
+      filteredEvents,
+      date,
+    },
+  };
+};
 
 export default FilteredEventsPage;
